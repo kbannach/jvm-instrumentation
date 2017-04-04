fcc31a5350e70e9799d96858a9120a4cd68dcf1c
kbannach <krzysztof92b@gmail.com>
2017-04-04 22:03:28 +0200

    adding time measuring code to methods decorated with @Measure

--- src/main/java/instr/ExecutionTimeTransformer.java
+++ src/main/java/instr/ExecutionTimeTransformer.java
@@ -6,6 +6,7 @@ import java.security.ProtectionDomain;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
+import javassist.NotFoundException;
 import service.Measure;
 
 public class ExecutionTimeTransformer implements ClassFileTransformer {
@@ -13,22 +14,27 @@ public class ExecutionTimeTransformer implements ClassFileTransformer {
    @Override
    public byte[] transform(ClassLoader loader, String className, Class< ? > classBeingRedefined, ProtectionDomain protectionDomain, byte[] bytes) throws IllegalClassFormatException {
       byte[] result = bytes;
-      if (classBeingRedefined.getAnnotation(Measure.class) != null) {
-         try {
-            String dotClassName = className.replace('/', '.');
-            ClassPool cp = ClassPool.getDefault();
-            CtClass ctClazz = cp.get(dotClassName);
-            CtMethod method1 = ctClazz.getDeclaredMethod("doSomething");
-            method1.addLocalVariable("elapsedTime", CtClass.longType);
-            method1.insertBefore("elapsedTime = System.currentTimeMillis();");
-            method1.insertAfter(String.format(
-                  " { elapsedTime = System.currentTimeMillis() - elapsedTime; System.out.println(\" Method %s from class %s execution time = \" + elapsedTime + \" (actual arguments: $1, $2)\"); } ",
-                  method1.getName(),
-                  ctClazz.getName()));
-            result = ctClazz.toBytecode();
-         } catch (Throwable e) {
-            e.printStackTrace();
+      try {
+         if (className == null)
+            return result;
+         String dotClassName = className.replace('/', '.');
+         ClassPool cp = ClassPool.getDefault();
+         CtClass ctClazz = cp.get(dotClassName);
+         for (CtMethod m : ctClazz.getDeclaredMethods()) {
+            if (m.getAnnotation(Measure.class) != null) {
+               m.addLocalVariable("elapsedTime", CtClass.longType);
+               m.insertBefore("elapsedTime = System.currentTimeMillis();");
+               m.insertAfter(String.format(
+                     " { elapsedTime = System.currentTimeMillis() - elapsedTime; System.out.println(\" Method %s from class %s execution time = \" + elapsedTime + \" (actual arguments: $1, $2)\"); } ",
+                     m.getName(),
+                     ctClazz.getName()));
+               result = ctClazz.toBytecode();
+            }
          }
+      } catch (NotFoundException e) {
+         // ignore
+      } catch (Throwable e) {
+         e.printStackTrace(System.out);
       }
       return result;
    }
